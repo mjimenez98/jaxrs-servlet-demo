0159a0bc0374a963708351177a3743db2b1bb5f6
Miguel Jimenez <migueleduardo298@hotmail.com>
2021-02-08 21:55:14 -0400

    Create a module for each API

---
+++ artist-api/src/main/java/service/ArtistsServlet.java
@@ -0,0 +1,27 @@
+package service;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "artistsServlet", value = "/artists")
+public class ArtistsServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
